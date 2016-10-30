0bb1b73893acccffbf3883ef2830a7a653192099
Quanyong Huang <quanyongh@foxmail.com>
2016-10-30 13:35:43 +0800

    add: alpha_bleeding && alpha_remove

--- PyTexturePacker/Utils.py
+++ PyTexturePacker/Utils.py
@@ -46,3 +46,72 @@ def save_plist(data_dict, file_name):
 
 def save_image(image, file_name):
     image.save(file_name)
+
+
+def alpha_bleeding(image, bleeding_pixel = 32):
+    offsets = ((-1, -1), (0, -1), (1, -1),
+               (-1, 0), (1, 0),
+               (-1, 1), (0, 1), (1,1))
+
+    image = image.copy()
+    width, height = image.size
+    if image.mode != "RGBA":
+        image = image.convert("RGBA")
+    pa = image.load()
+
+    bleeding = set()
+    borders = []
+
+    def _tell_border(x, y):
+        if pa[x, y][3] == 0:
+            return False
+
+        for offset in offsets:
+            ox = x + offset[0]
+            oy = y + offset[1]
+            if ox >= 0 and ox < width and oy >= 0 and oy < height \
+                    and pa[ox, oy][3] == 0:
+                return True
+        return False
+
+    def _bleeding(x ,y):
+        borders = []
+        pixel = pa[x, y]
+        for offset in offsets:
+            ox = x + offset[0]
+            oy = y + offset[1]
+            if ox >= 0 and ox < width and oy >= 0 and oy < height \
+                    and pa[ox, oy][3] == 0 and (ox, oy) not in bleeding:
+                pa[ox, oy] = (pixel[0], pixel[1], pixel[2], 255)
+                bleeding.add(pa)
+                if _tell_border(ox, oy):
+                    borders.append((ox, oy))
+        return borders
+
+    for x in range(width):
+        for y in range(height):
+            if _tell_border(x, y):
+                borders.append((x, y))
+
+    for i in range(bleeding_pixel):
+        pending = []
+        for border in borders:
+            pending.extend(_bleeding(*border))
+        borders = pending
+
+    return image
+
+
+def alpha_remove(image):
+    image = image.copy()
+    width, height = image.size
+    if image.mode != "RGBA":
+        image = image.convert("RGBA")
+    pa = image.load()
+    for x in range(width):
+        for y in range(height):
+            pixel = pa[x,y]
+            pa[x, y] = (pixel[0], pixel[1], pixel[2], 255)
+    return image
+
+
