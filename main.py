ebfe94cb503bc78dd91f0ba1192bdfd2b14f5086
Quanyong Huang <quanyongh@foxmail.com>
2016-11-05 19:23:41 +0800

    mod: test_case -> test_image

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from PyTexturePacker import Packer
 
 def pack_test():
     packer = Packer.create(max_width=2048, max_height=2048, bg_color=0xffffff00, trim_mode=1)
-    packer.pack("test_case/", "test_case%d")
+    packer.pack("test_image/", "test_image%d")
 
 
 def main():
