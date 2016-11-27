14cc7a46f1d7d4a439738e620b856aeaa4e2d678
wo1fsea <quanyongh@foxmail.com>
2016-11-27 23:38:59 +0800

    add: setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='PyTexturePacker',
+    packages=['PyTexturePacker'],
+    version='0.1',
+    description='an package to create sprite sheets or sprite atlases',
+    author='Quanyong Huang',
+    author_email='quanyongh@foxmail.com',
+    url='https://github.com/wo1fsea/PyTexturePacker',
+    download_url='https://github.com/wo1fsea/PyTexturePacker',
+    keywords=['TexturePacker', 'cocos2d'],  
+    classifiers=[],
+)
