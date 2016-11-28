acff201639058ec8bcc45cc3a907df3958864ebb
wo1fsea <quanyongh@foxmail.com>
2016-11-28 23:45:00 +0800

    mod: renew setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='PyTexturePacker',
-    packages=['PyTexturePacker'],
-    version='0.1',
+    packages=find_packages(exclude=['docs', 'tests', 'test_image']),
+    version='0.0.1',
     description='an package to create sprite sheets or sprite atlases',
     author='Quanyong Huang',
     author_email='quanyongh@foxmail.com',
     url='https://github.com/wo1fsea/PyTexturePacker',
     download_url='https://github.com/wo1fsea/PyTexturePacker',
-    keywords=['TexturePacker', 'cocos2d'],  
+    keywords=['TexturePacker', 'cocos2d'],
     classifiers=[],
+    license='MIT',
+    install_requires=['Pillow'],
 )
