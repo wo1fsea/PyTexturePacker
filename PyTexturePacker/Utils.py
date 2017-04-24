f714d9c7d9909fed762aa82f1b9ec575fde0baaa
黄铨雍 <huangquanyong@Huang-Quanyongs-Air.local>
2017-04-24 23:51:30 +0800

    fix: alpha_bleeding

--- PyTexturePacker/Utils.py
+++ PyTexturePacker/Utils.py
@@ -111,7 +111,7 @@ def alpha_bleeding(image, bleeding_pixel=8):
             ox = x + offset[0]
             oy = y + offset[1]
             if 0 <= ox < width and 0 <= oy < height and pa[ox, oy][3] == 0 and (ox, oy) not in bleeding:
-                pa[ox, oy] = (pixel[0], pixel[1], pixel[2], 0)
+                pa[ox, oy] = (pixel[0], pixel[1], pixel[2], 1)
                 bleeding.add(pa)
                 if _tell_border(ox, oy):
                     borders.append((ox, oy))
