eded60a3e575e8f30654d75d15c4fe2f78232f25
huang quanyong <quanyongh@foxmail.com>
2024-02-14 21:32:33 +0800

    mod long desc

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     packages=find_packages(exclude=['docs', 'tests', 'test_image']),
     version='1.2.0',
     description='an package to create sprite sheets or sprite atlases',
+    long_description_content_type="text/x-rst",
+    long_description=open('README.rst').read(),
     author='Quanyong Huang',
     author_email='quanyongh@foxmail.com',
     url='https://github.com/wo1fsea/PyTexturePacker',
